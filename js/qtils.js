5f654ff639b10509611f32dd1f3532537adf1068
context-dependent <tmcmanus@BPA.LOCAL>
2019-07-17 15:51:02 -0400

    noc autocomplete works

--- js/qtils.js
+++ js/qtils.js
@@ -2,7 +2,7 @@
 
 	const qtils = {
 
-		autocomplete_text_input : function(input_id, data_list_url) {
+		autocomplete_text_input : function(question_id, input_id, data_list_url) {
 	
 			fetch(data_list_url)
 				.then(function(response) {
@@ -16,6 +16,27 @@
 						filter: function(text, input) {
 							var res = text.value.match(input) || text.label.indexOf(input) === 0;
 							return res;
+						},
+						replace: function(text) {
+							this.input.value = text.label;
+						}, 
+						container: function() {
+							
+							var question_container = document.getElementById(question_id); 
+							var awesomplete_div = document.createElement("div");
+							
+							awesomplete_div.addClassName("awesomplete");
+							awesomplete_div.id = question_id + "-awesomplete";
+							awesomplete_div.style.marginLeft = "5%";
+							awesomplete_div.style.position = "absolute";
+							awesomplete_div.style.width = "85%";
+							
+							question_container.appendChild(awesomplete_div);
+							
+							
+							
+							return  document.getElementById(question_id + "-awesomplete");
+							
 						}
 					});
 				});
